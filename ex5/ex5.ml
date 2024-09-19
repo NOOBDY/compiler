cb2f12cb59354b5da658370b000d50d30e214b14
noobdy <htommyf12@gmail.com>
2024-09-19 16:41:07 +0800

    format

--- ex5/ex5.ml
+++ ex5/ex5.ml
@@ -7,36 +7,40 @@ let find_opt v l =
   let rec aux l' i =
     match l' with
     | [] -> None
-    | x :: xs ->
-        if v = x
-          then Some v
-          else aux xs (i + 1)
+    | x :: xs -> if v = x then Some v else aux xs (i + 1)
   in
-    aux l 0
+  aux l 0
+;;
 
 let find_opt' x l =
   let aux (res, i) v =
-      match res with
-      | None when x == v -> (Some i, i + 1)
-      | _ -> (res, i + 1)
+    match res with
+    | None when x == v -> Some i, i + 1
+    | _ -> res, i + 1
   in
-    fst (List.fold_left aux (None, 0) l)
+  fst (List.fold_left aux (None, 0) l)
+;;
 
 let () =
-    let list = [38; 27; 43; 3; 9; 82; 10] in
-    Printf.printf "%d" (square_sum list);
-      print_newline ()
+  let list = [ 38; 27; 43; 3; 9; 82; 10 ] in
+  Printf.printf "%d" (square_sum list);
+  print_newline ()
+;;
 
 let () =
-    let list = [38; 27; 43; 3; 9; 82; 10] in
-    match (find_opt 43 list) with
-    | Some v -> print_int v;
-      print_newline ()
-    | None -> print_endline "Not found"
+  let list = [ 38; 27; 43; 3; 9; 82; 10 ] in
+  match find_opt 43 list with
+  | Some v ->
+    print_int v;
+    print_newline ()
+  | None -> print_endline "Not found"
+;;
 
 let () =
-    let list = [38; 27; 43; 3; 9; 82; 10] in
-    match (find_opt' 43 list) with
-    | Some v -> print_int v;
-      print_newline ()
-    | None -> print_endline "Not found"
+  let list = [ 38; 27; 43; 3; 9; 82; 10 ] in
+  match find_opt' 43 list with
+  | Some v ->
+    print_int v;
+    print_newline ()
+  | None -> print_endline "Not found"
+;;
--- ex6/ex6.ml
+++ ex6/ex6.ml
@@ -1,19 +1,21 @@
-let l = List.init 1_000_000 ((+)1)
+let l = List.init 1_000_000 (( + ) 1)
 
 let rev l =
-    let rec aux l1 l2 =
-        match l1 with
-        | [] -> l2
-        | x :: xs -> aux xs (x :: l2)
-    in
-        aux l []
+  let rec aux l1 l2 =
+    match l1 with
+    | [] -> l2
+    | x :: xs -> aux xs (x :: l2)
+  in
+  aux l []
+;;
 
 let rec map f l =
-    match l with
-    | [] -> []
-    | x :: xs -> f x :: map f xs
+  match l with
+  | [] -> []
+  | x :: xs -> f x :: map f xs
+;;
 
 let () = List.iter (Printf.printf "%d ") (rev l)
 let () = print_newline ()
-let () = List.iter (Printf.printf "%d ") (map ((+)2) l)
+let () = List.iter (Printf.printf "%d ") (map (( + ) 2) l)
 let () = print_newline ()
