8fdacd0d32db5753c7ae7c1bc9f6b9eb9fada30a
noobdy <htommyf12@gmail.com>
2024-10-08 16:25:31 +0800

    reset

--- hw0/ex1/ex1.ml
+++
@@ -1,9 +0,0 @@
-(* (a) *)
-let rec fact n = if n = 0 then 1 else n * fact (n - 1)
-
-(* (b) *)
-let rec nb_bit_pos n = if n = 0 then 0 else (n land 1) + nb_bit_pos (n lsr 1)
-let () = print_int (fact 10)
-let () = print_string "\n"
-let () = print_int (nb_bit_pos 9)
-let () = print_string "\n"
